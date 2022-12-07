1c401ee2218013e2158ec6e1f2953db12f3da4c3
Oksana-Zakhariia <okcahka.l@gmail.com>
2022-12-07 22:06:01 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const API_KEY = 'c65b1581ed69fd3b6701ed620730aaec'
 const BASE_URL = 'https://api.themoviedb.org/3/trending/movie/day'
-const list = document.querySelector('.list')
+const list = document.querySelector('.list');
+const modal = document.querySelector('.modal')
+
 
 // https://api.themoviedb.org/3/trending/movie/day?api_key=c65b1581ed69fd3b6701ed620730aaec
 async function moviesApi() {
@@ -37,13 +39,36 @@ function onListClick(event) {
    
     const filmId= event.target.parentNode.querySelector('.id').textContent
     console.log(filmId);
-    onFilmCardClick().then((data)=>{console.log(data);})
+    onFilmCardClick(filmId).then((data) => {
+         console.log(data)
+        createFilmModalCard(data)
+       
+    })
     
 }
  async function onFilmCardClick(filmId) {
-    const response = fetch(`https://api.themoviedb.org/3/movie/${filmId}?api_key=${API_KEY}`)
+    const response = await fetch(`https://api.themoviedb.org/3/movie/${filmId}?api_key=${API_KEY}`)
         if (!response.ok) {
         throw new Error
     }
     return await response.json(); 
-    }
+}
+function createFilmModalCard(data) {
+    const markup = `<img src="https://image.tmdb.org/t/p/original/${data.poster_path}
+" alt="${data.original_title}" width="375px" height="478px">
+    <div class="modal__info">
+    <h1 class="modal__title"> ${data.title} </h1>
+    <ul class="modal__statistic-list">
+    <li class="modal__statistic-item"> Vote / Votes <span>${data.vote_average}/ ${data.vote_count}</span></li>
+    <li class="modal__statistic-item"> Popularity <span>${data.popularity}</span></li>
+    <li class="modal__statistic-item">Original Title <span>${data.original_title}</span></li>
+    <li class="modal__statistic-item">Genre <span>${data.genres.map(({name})=>{Object.values(name)})}</span></li>
+    </ul>
+    <h2 class="modal__subtitle">ABOUT</h2>
+    <p class="modal__text">${data.overview}</p>
+    <button class="modal__adding-button"> ADD TO WATCHED </button>
+    <button class="modal__queue-button"> ADD TO QUEUE</button>
+    </div> `;
+   
+    modal.insertAdjacentHTML('beforeend', markup)
+}
