abcef213b9497452669588cad0ecde27f953bee7
Oksana-Zakhariia <okcahka.l@gmail.com>
2022-12-15 23:02:24 +0300

    hgjlik

--- src/index.js
+++ src/index.js
@@ -52,6 +52,7 @@ function onListClick(event) {
     console.log(filmId);
    
     onFilmCardClick(filmId).then((data) => {
+        console.log(data);
         body.classList.add('show-modal')
         window.addEventListener('keydown', onEscKeyPress)
         createFilmModalCard(data)       
@@ -66,22 +67,43 @@ function onListClick(event) {
     return await response.json(); 
 }
 function createFilmModalCard(data) {
-    const markup = `<img src="https://image.tmdb.org/t/p/original/${data.poster_path}
-" alt="${data.original_title}" width="375px" height="478px">
-    <div class="modal__info">
-    <h1 class="modal__title"> ${data.title} </h1>
-    <ul class="modal__statistic-list">
-    <li class="modal__statistic-item"> Vote / Votes <span>${data.vote_average}/ ${data.vote_count}</span></li>
-    <li class="modal__statistic-item"> Popularity <span>${data.popularity}</span></li>
-    <li class="modal__statistic-item">Original Title <span>${data.original_title}</span></li>
-    <li class="modal__statistic-item">Genre <span>${data.genres.map((item)=>{return item['name']})}</span></li>
-    </ul>
-    <h2 class="modal__subtitle">ABOUT</h2>
-    <p class="modal__text">${data.overview}</p>
-    <button class="modal__adding-button"> ADD TO WATCHED </button>
-    <button class="modal__queue-button"> ADD TO QUEUE</button>    
-   
-    </div> `;
+    const markup = `<img class="modal__card-img img" src="https://image.tmdb.org/t/p/original/${data.poster_path}" alt="${data.original_title}" 
+        width="375" height="478"> 
+      <div class="modal__card"> 
+        <h3 class="modal__card-title">${data.title}</h3> 
+        <ul class="modal__card-list list"> 
+          <li class="card__item"> 
+            <h4 class="card__item-title">Vote / Votes</h4> 
+            <p class="card__item-vote">${data.vote_average}</p> 
+            <span class="card__item-slash">/</span> 
+            <p class="card__item-votes">${data.vote_count}</p> 
+ 
+          <li class="card__item"> 
+            <h4 class="card__item-title">Popularity</h4> 
+            <p class="card__item-popularity">${data.popularity}</p> 
+          </li> 
+          <li class="card__item"> 
+            <h4 class="card__item-title">Original Title</h4> 
+            <p class="card__item-original">${data.original_title
+        }</p> 
+          </li> 
+          <li class="card__item"> 
+            <h4 class="card__item-title">Genre</h4> 
+            <p class="card__item-genre">${data.genres.map((item) => { return item['name'] })}</p> 
+          </li> 
+        </ul> 
+        <h4 class="card__item-about">About</h4> 
+        <p class="card__item-about__text card__item-text">${data.overview}</p> 
+ 
+        <ul class="card__btn-list list"> 
+          <li class="card__btn-item"> 
+            <button class="card__btn">add to Watched</button> 
+          </li> 
+          <li class="card__btn-item"> 
+            <button class="card__btn">add to queue</button> 
+          </li> 
+        </ul> 
+      </div> `;
    
     modal.insertAdjacentHTML('beforeend', markup)
 }
